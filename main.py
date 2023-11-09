88fb4436b108f8ee79d576c4073da9c6b1d58865
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-11-09 13:03:23 +0000

    added cors

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from app.core.deps import create_superuser
 from app.core.settings import settings
 from app.api.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def on_startup():
