0d4fd27d3f35d48d3922d1f22076acbf9d637037
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-11-14 07:22:18 +0000

    Update course.py

--- app/schemas/course.py
+++ app/schemas/course.py
@@ -4,25 +4,17 @@ from sqlmodel import Relationship, SQLModel, Field, Column, String
 from sqlalchemy import DateTime
 from sqlalchemy.sql import func
 
-
-
-
 class CourseBase(SQLModel):
     name : str = Field(
         index=True, sa_column=Column("name", String, unique=True))
     university_id : int = Field(foreign_key="university.id", default=None)
   
-
 class CourseCreate(CourseBase):
     pass
-
 
 class CourseRead(CourseBase):
     id: int
 
-
 class CourseUpdate(SQLModel): 
     name: Optional[str] = None
     university_id : Optional[int] = None
-
-
