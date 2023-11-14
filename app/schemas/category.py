53c5b8f41bed90a96c2762f6c0b0002de1596bb0
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-11-14 07:22:53 +0000

    Update category.py

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -7,24 +7,17 @@ from sqlalchemy.sql import func
 if TYPE_CHECKING:
     from app.models.resource import Resource
 
-
 class CategoryBase(SQLModel):
     name : str = Field(
         index=True, sa_column=Column("name", String, unique=True))
     resources: List["Resource"] = Relationship(back_populates="category")
 
-
 class CategoryCreate(SQLModel):
     name : str = Field(
         index=True, sa_column=Column("course_code", String, unique=True))
 
-
-
 class CategoryRead(CategoryBase):
     id: int
 
-
 class CategoryUpdate(SQLModel): 
     name: Optional[str] = None
-
-
