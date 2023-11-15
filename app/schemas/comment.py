e66982ff247ab4c5ebfe98d7069e1ef3bdf25310
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-11-15 17:07:41 +0000

    Update comment.py

--- app/schemas/comment.py
+++ app/schemas/comment.py
@@ -6,11 +6,9 @@ from sqlalchemy.sql import func
 from app.models.resource import Resource
 
 class CommentBase(SQLModel):
-    text : str = Field(
-        index=True, sa_column=Column("text", String, unique=True))
+    text : str = Field(index=True, sa_column=Column("text", String, unique=True))
     resource_id : int = Field(foreign_key="resource.id", default=None)
-    created_at: Optional[datetime] = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now()))
+    created_at: Optional[datetime] = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now()))
     resource: Resource = Relationship(back_populates="comment")
 
 class CommentCreate(CommentBase):
@@ -21,6 +19,3 @@ class CommentRead(CommentBase):
 
 class CommentUpdate(SQLModel): 
     text: Optional[str] = None
-
-
-
